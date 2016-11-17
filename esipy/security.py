3af6775b89c756cfc2ad534f9f5d750acbe9d46a
Kyria <anakhon@gmail.com>
2016-11-17 15:53:28 +0100

    Add security module that manage all the OAuth2 knowledge

---
+++ esipy/security.py
@@ -0,0 +1,147 @@
+# -*- encoding: utf-8 -*-
+from requests.utils import quote
+import six
+
+
+class EsiSecurity(object):
+    """ Contains all the OAuth2 knowledge for ESI use.
+    Based on pyswagger Security object, to be used with pyswagger BaseClient
+    implementation.
+    """
+
+    def __init__(
+            self,
+            app,
+            redirect_uri,
+            client_id,
+            secret_key,
+            security_name="evesso"):
+        """ Init the ESI Security Object
+
+        :param app: the pyswagger app object
+        :param redirect_uri: the uri to redirect the user after login into SSO
+        :param client_id: the OAuth2 client ID
+        :param secret_key: the OAuth2 secret key
+        :param security_name: (optionnal) the name of the object holding the
+        informations in the securityDefinitions.
+
+        """
+        # check if the security_name actually exists in the securityDefinition
+        if app.root.securityDefinitions.get(security_name, None) is None:
+            raise NameError(
+                "%s is not defined in the securityDefinitions" % security_name
+            )
+
+        self.app = app
+        self.security_name = security_name
+        self.redirect_uri = redirect_uri
+        self.client_id = client_id
+        self.secret_key = secret_key
+
+        # token data
+        self.refresh_token = None
+        self.access_token = None
+        self.token_expiry = None
+
+    def get_auth_uri(self, scopes=None, state=None):
+        """ Constructs the full auth uri and returns it.
+
+        :param scopes: The list of scope
+        :param state: The state to pass through the auth process
+
+        """
+        security_definition = self.app.root.securityDefinitions.get(
+            self.security_name
+        )
+
+        return "%s?response_type=code&redirect_uri=%s&client_id=%s%s%s" % (
+            security_definition.authorizationUrl,
+            quote(self.redirect_uri, safe=''),
+            self.client_id,
+            "&scope=%s" % '+'.join(s) if scopes else '',
+            "&state=%s" % state if state else ''
+        )
+
+    def get_token_uri(self, testing=False):
+        """ Return the token uri from the securityDefinition
+
+        :param testing: True if we are on Singularity, else false for TQ.
+        """
+        security_definition = self.app.root.securityDefinitions.get(
+            self.security_name
+        )
+
+        # should be: security_definition.tokenUrl
+        # but not yet implemented by CCP in ESI so we need the full URL...
+        # https://github.com/ccpgames/esi-issues/issues/92
+        if testing:
+            return "https://sisilogin.testeveonline.com/oauth"
+        else:
+            return "https://login.eveonline.com/oauth/token"
+
+    def get_token_auth_header(self):
+        """ Return the Basic Authorization header required to get the tokens
+
+        :return a dict with the headers
+        """
+        # encode/decode for py2/py3 compatibility
+        auth_b64 = "%s:%s" % (self.client_id, self.secret_key)
+        auth_b64 = base64.b64encode(auth_b64.encode('latin-1'))
+        auth_b64 = auth_b64.decode('latin-1')
+
+        return {"Authorization": "Basic %s" % auth_b64}
+
+    def apply_auth_header(self, request):
+        """ Apply the Bearer Authorization header for ESI calls.
+        This method is used in the __call__ function.
+
+        :param request: the pyswagger request object where we update the header
+        """
+        if self.access_token is not None:
+            request._p['header'].update({
+                "Authorization": "Bearer %s" % self.access_token
+            })
+        return request
+
+    def update_token(self, response_json):
+        """ Update access_token, refresh_token and token_expiry from the
+        response body.
+        The response must be converted to a json object before being passed as
+        a parameter
+
+        :param response_json: the response body to use.
+        """
+        self.access_token = response_json['access_token']
+        self.token_expiry = int(time.time()) + response_json['expires_in']
+
+        if "refresh_token" in response_json:
+            self.refresh_token = response_json['refresh_token']
+
+    def is_token_expired(self, offset=0):
+        """ Return true if the token is expired.
+        The offset can be used to change the expiry time:
+        - positive value decrease the time (sooner)
+        - negative value increase the time (later)
+
+        :param offset: the expiry offset (in seconds) [default: 0]
+        """
+        return int(time.time()) >= (self.token_expiry - offset)
+
+    def __call__(self, request):
+        """ Check if the request need security header and apply them.
+        Call by client.request.
+
+        :param request: the pyswagger request object to check
+        """
+        if not request._security:
+            return request
+
+        for security in request._security:
+            if self.security_name not in security:
+                logger.warning(
+                    'Missing Securities: [{0}]' % ', '.join(security.keys())
+                )
+                continue
+            apply_auth_header(request)
+
+        return req

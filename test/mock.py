34db9276e7e7ef2e0c54d21c11c78eefe4a2bde5
Kyria <anakhon@gmail.com>
2016-11-29 08:28:06 +0100

    Add tests: currently only cache class is tested. Also add all mocks in a single file to be imported where needed.

---
+++ test/mock.py
@@ -0,0 +1,118 @@
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+
+import datetime
+import httmock
+import json
+
+
+def make_expire_time_str():
+    """ Generate a date string for the Expires header
+    RFC 7231 format
+    """
+    date = datetime.datetime.utcnow()
+    date += datetime.timedelta(days=1)
+    return date.strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+
+@httmock.urlmatch(
+    scheme="https",
+    netloc=r"esi\.tech\.ccp\.is$",
+    path=r"^/latest/swagger.json$"
+)
+def swagger_json_mock(url, request):
+    """ Root Mock for the tests"""
+    with open('test/resources/swagger.json') as file:
+        data = json.load(file)
+
+    return httmock.response(
+        headers={'Expires': make_expire_time_str()},
+        status_code=200,
+        content=data
+    )
+    
+
+def oauth_token(url, request):
+    """ Mock endpoint to get token (auth / refresh) """
+    pass
+
+
+def oauth_verify(url, request):
+    """ Mock endpoint to get character data after auth """
+    pass
+
+    
+@httmock.urlmatch(
+    scheme="https",
+    netloc=r"esi\.tech\.ccp\.is$",
+    path=r"^/latest/incursions/$"
+)
+def public_incursion(url, request):
+    """ Mock endpoint for incursion.
+    Public endpoint
+    """
+    return httmock.response(
+        headers={'Expires': make_expire_time_str(), 'content-type': 'application/json'},
+        status_code=200,
+        content=[
+            {
+                "type": "Incursion",
+                "state": "mobilizing",
+                "staging_solar_system_id": 30003893,
+                "constellation_id": 20000568,
+                "infested_solar_systems": [
+                    30003888,
+                ],
+                "has_boss": True,
+                "faction_id": 500019,
+                "influence": 1
+            }
+        ]
+    )
+        
+        
+@httmock.urlmatch(
+    scheme="https",
+    netloc=r"esi\.tech\.ccp\.is$",
+    path=r"^/latest/incursions/$"
+)
+def public_incursion_no_expires(url, request):
+    """ Mock endpoint for incursion.
+    Public endpoint without cache
+    """
+    return httmock.response(
+        status_code=200,
+        content=[
+            {
+                "type": "Incursion",
+                "state": "mobilizing",
+                "staging_solar_system_id": 30003893,
+                "constellation_id": 20000568,
+                "infested_solar_systems": [
+                    30003888,
+                ],
+                "has_boss": True,
+                "faction_id": 500019,
+                "influence": 1
+            }
+        ]
+    )
+    
+
+@httmock.urlmatch(
+    scheme="https",
+    netloc=r"esi\.tech\.ccp\.is$",
+    path=r"^/latest/characters/(\d+)/location/$"
+)
+def auth_character_location(url, request):
+    """ Mock endpoint for character location.
+    Authed endpoint that check for auth
+    """
+    return httmock.response(
+        headers={'Expires': make_expire_time_str()},
+        status_code=200,
+        content={
+            "station_id": 60004756,
+            "solar_system_id": 30002543
+        }
+    )
---
+++ test/test_cache.py
@@ -0,0 +1,166 @@
+# -*- encoding: utf-8 -*-
+# -- Tests based on pycrest cache tests (as these are the same classes)
+# -- https://github.com/pycrest/PyCrest/blob/master/tests/test_pycrest.py
+from __future__ import absolute_import
+
+import errno
+import httmock
+import mock
+import sys
+import six
+import unittest
+import memcache
+
+from esipy.cache import BaseCache
+from esipy.cache import DictCache
+from esipy.cache import DummyCache
+from esipy.cache import FileCache
+from esipy.cache import MemcachedCache
+from requests.adapters import HTTPAdapter
+from requests.models import PreparedRequest
+from six.moves import builtins
+
+
+class TestBaseCache(unittest.TestCase):
+    """ BaseCache test class """
+    
+    def setUp(self):
+        self.c = BaseCache()
+
+    def test_base_cache_put(self):
+        self.assertRaises(NotImplementedError, self.c.get, 'key')
+
+    def test_base_cache_get(self):
+        self.assertRaises(NotImplementedError, self.c.put, 'key', 'val')
+
+    def test_base_cache_invalidate(self):
+        self.assertRaises(NotImplementedError, self.c.invalidate, 'key')
+
+
+class TestDictCache(unittest.TestCase):
+    """ DictCache test class """
+    
+    def setUp(self):
+        self.c = DictCache()
+        self.c.put('key', True)
+
+    def test_dict_cache_put(self):
+        self.assertEqual(self.c._dict['key'], True)
+
+    def test_dict_cache_get(self):
+        self.assertEqual(self.c.get('key'), True)
+
+    def test_dict_cache_invalidate(self):
+        self.c.invalidate('key')
+        self.assertIsNone(self.c.get('key'))
+        
+        
+class TestDummyCache(unittest.TestCase):
+    """ DummyCache test class. """
+    
+    def setUp(self):
+        self.c = DummyCache()
+        self.c.put('never_stored', True)
+
+    def test_dummy_cache_put(self):
+        self.assertNotIn('never_stored', self.c._dict)
+
+    def test_dummy_cache_get(self):
+        self.assertEqual(self.c.get('never_stored'), None)
+
+    def test_dummy_cache_invalidate(self):
+        self.c.invalidate('never_stored')
+        self.assertIsNone(self.c.get('never_stored'))
+
+
+class TestFileCache(unittest.TestCase):
+    """ Class for testing the filecache """
+
+    DIR = '/tmp/TestFileCache'
+
+    @mock.patch('os.path.isdir')
+    @mock.patch('os.mkdir')
+    @mock.patch('{0}.open'.format(builtins.__name__))
+    def setUp(self, open_function, mkdir_function, isdir_function):
+        self.c = FileCache(TestFileCache.DIR)
+        self.c.put('key', 'value')
+
+    @mock.patch('os.path.isdir', return_value=False)
+    @mock.patch('os.mkdir')
+    def test_file_cache_init(self, mkdir_function, isdir_function):
+        c = FileCache(TestFileCache.DIR)
+
+        # Ensure path has been set
+        self.assertEqual(c.path, TestFileCache.DIR)
+
+        # Ensure we checked if the dir was already there
+        args, kwargs = isdir_function.call_args
+        self.assertEqual((TestFileCache.DIR,), args)
+
+        # Ensure we called mkdir with the right args
+        args, kwargs = mkdir_function.call_args
+        self.assertEqual((TestFileCache.DIR, 0o700), args)
+
+    def test_file_cache_get_uncached(self):
+        # Check non-existant key
+        self.assertIsNone(self.c.get('nope'))
+
+    @mock.patch('{0}.open'.format(builtins.__name__))
+    def test_file_cache_get_cached(self, open_function):
+        self.assertEqual(self.c.get('key'), 'value')
+
+    @unittest.skipIf(six.PY2, 'Python 2.x uses a diffrent protocol')
+    @mock.patch('{0}.open'.format(builtins.__name__), mock.mock_open(
+        read_data=b'x\x9ck`\ne-K\xcc)M-d\xd0\x03\x00\x17\xde\x03\x99'))
+    def test_file_cache_get_cached_file_py3(self):
+        del(self.c._cache['key'])
+        self.assertEqual(self.c.get('key'), 'value')
+
+    @unittest.skipIf(six.PY3, 'Python 3.x uses a diffrent protocol')
+    @mock.patch('{0}.open'.format(builtins.__name__), mock.mock_open(
+        read_data='x\x9ck`\ne-K\xcc)M-d\xd0\x03\x00\x17\xde\x03\x99'))
+    def test_file_cache_get_cached_file_py2(self):
+        del(self.c._cache['key'])
+        self.assertEqual(self.c.get('key'), 'value')
+
+    @mock.patch('os.unlink')
+    def test_file_cache_invalidate(self, unlink_function):
+        # Make sure our key is here in the first place
+        self.assertIn('key', self.c._cache)
+
+        # Unset the key and ensure unlink() was called
+        self.c.invalidate('key')
+        self.assertTrue(unlink_function.called)
+
+    @mock.patch(
+        'os.unlink',
+        side_effect=OSError(
+            errno.ENOENT,
+            'No such file or directory')
+    )
+    def test_file_cache_unlink_exception(self, unlink_function):
+        self.assertIsNone(self.c.invalidate('key'))
+
+
+class TestMemcachedCache(unittest.TestCase):
+    """A very basic MemcachedCache TestCase
+    Primairy goal of this unittest is to get the coverage up
+    to spec. Should probably make use of `mockcache` in the future"""
+
+    def setUp(self):
+        memcached = memcache.Client(['127.0.0.1:11211'], debug=0)
+        memcached.get = mock.MagicMock(return_value='value')
+        self.c = MemcachedCache(memcached)
+
+    def test_memcached_put(self):
+        self.c.put('key', 'value')
+
+    def test_memcached_get(self):
+        self.assertEqual(self.c.get('key'), 'value')
+
+    def test_memcached_invalidate(self):
+        self.c.invalidate('key')
+
+        
+if __name__ == "__main__":
+    unittest.main()

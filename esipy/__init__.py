1479cd0e91f18d971a57c33971171a7d7c1a6019
Kyria <anakhon@gmail.com>
2016-12-16 11:05:31 +0100

    add import in __init__.py for easier use (from esipy import App, EsiClient, EsiSecurity)

--- esipy/__init__.py
+++ esipy/__init__.py
@@ -1,3 +1,10 @@
 # -*- encoding: utf-8 -*-
+from __future__ import absolute_import
 
-__version__ = "0.0.1"
+from .client import EsiClient
+from .security import EsiSecurity
+
+from pyswagger import App
+
+__version__ = "0.0.2"
+__all__ = [EsiClient, EsiSecurity, App]

ea18ab430e49c5deb5a0c19fbda66cbaca8256c7
Guillaume <anakhon@gmail.com>
2016-12-21 00:52:22 +0100

    Move import in try except (forgot the others)

--- esipy/__init__.py
+++ esipy/__init__.py
@@ -1,10 +1,9 @@
 # -*- encoding: utf-8 -*-
 from __future__ import absolute_import
 
-from .client import EsiClient  # noqa
-from .security import EsiSecurity  # noqa
-
 try:
+    from .client import EsiClient  # noqa
+    from .security import EsiSecurity  # noqa
     from pyswagger import App  # noqa
 except Exception:  # pragma: no cover
     # Not installed or in install (not yet installed) so ignore

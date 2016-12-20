fa162d43c9500066c2804be139b7f08938eefba7
Guillaume <anakhon@gmail.com>
2016-12-21 00:44:21 +0100

    Fix setup and version in __init__
    Also add a try except on App to prevent error on setup (because of pyswagger not yet being installed)

--- esipy/__init__.py
+++ esipy/__init__.py
@@ -4,6 +4,11 @@ from __future__ import absolute_import
 from .client import EsiClient  # noqa
 from .security import EsiSecurity  # noqa
 
-from pyswagger import App  # noqa
+try:
+    from pyswagger import App  # noqa
+except Exception:  # pragma: no cover
+    # Not installed or in install (not yet installed) so ignore
+    pass
 
-__all__ = [EsiClient, EsiSecurity, App]
+
+__version__ = '0.0.5'
--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import esipy
+import io
+
 from setuptools import setup
 
 # install requirements
@@ -19,15 +22,19 @@ test_requirements = [
     "python-memcached"
 ] + install_requirements
 
+with io.open('README.md', encoding='UTF-8') as reader:
+    readme = reader.read()
+
 setup(
     name='EsiPy',
-    version='0.0.4',
+    version=esipy.__version__,
     packages=['esipy'],
     url='https://github.com/Kyria/EsiPy',
     license='BSD 3-Clause License',
     author='Kyria',
     author_email='anakhon@gmail.com',
     description='Swagger Client for the ESI API for EVE Online',
+    long_description=readme,
     install_requires=install_requirements,
     tests_require=test_requirements,
     test_suite='nose.collector',
@@ -36,10 +43,10 @@ setup(
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
     ]
 )

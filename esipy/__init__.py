8484032a89b42d98a9f0dd8ea0c3c7d88bb23fff
Guillaume <anakhon@gmail.com>
2017-12-25 16:50:07 +0100

    Add EsiApp shortcut in esipy.__init__

--- esipy/__init__.py
+++ esipy/__init__.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 try:
     from .client import EsiClient  # noqa
     from .security import EsiSecurity  # noqa
+    from .app import EsiApp  # noqa
     from pyswagger import App  # noqa
 except ImportError:  # pragma: no cover
             # Not installed or in install (not yet installed) so ignore
